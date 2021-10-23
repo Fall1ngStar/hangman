a922920e3228fd376cea30bba69396ce41b5d074
Fall1ngStar <12586447+Fall1ngStar@users.noreply.github.com>
2021-10-23 18:10:38 +0200

    Handle key press, check is game is lost

--- src/components/HangMan.vue
+++ src/components/HangMan.vue
@@ -2,32 +2,37 @@
     <h1>Hangman</h1>
     <div>
         <p>Find the word</p>
-        <p>{{ hiddenWord }}</p>
-        <p>Failed letters: {{ failedLetter }}</p>
-        <form @submit.prevent="handleForm">
-            <input type="text" v-model="currentLetter" maxlength="1" />
-            <button>Send letter</button>
-        </form>
-        <p v-if="hasWon"> You won !</p>
+        <p>{{ hiddenWord.join(" ") }}</p>
+        <p v-if="!hasWon">Failed letters: {{ failedLetter.join(" ") }}</p>
+        <p v-if="hasWon">You won !</p>
+        <p v-if="hasLost">You lost :(</p>
         <button @click="reset">Reset game</button>
     </div>
 </template>
 
 <script setup>
-import { computed, reactive, ref } from "vue";
+import { computed, reactive, ref, onMounted } from "vue";
 
 const toFind = ref("baguette");
 const currentLetter = ref("");
 const submittedLetters = reactive([]);
 
-const hiddenWord = computed(() => toFind.value.split("").map(letter => submittedLetters.includes(letter) ? letter : "_").join(" "))
-const failedLetter = computed(() => submittedLetters.filter(letter => !toFind.value.includes(letter)).sort().join(" "))
+const hiddenWord = computed(() => toFind.value.split("").map(letter => submittedLetters.includes(letter) ? letter : "_"))
+const failedLetter = computed(() => submittedLetters.filter(letter => !toFind.value.includes(letter)).sort())
 const hasWon = computed(() => toFind.value.split("").filter(letter => !submittedLetters.includes(letter)).length === 0)
+const hasLost = computed(() => failedLetter.value.length >= 8)
 
-const handleForm = () => {
-    if (!submittedLetters.includes(currentLetter.value)) submittedLetters.push(currentLetter.value);
-    currentLetter.value = "";
+const isLetter = (c) => c.toLowerCase() != c.toUpperCase()
+
+const handleKeyPress = event => {
+    if (hasLost.value || hasWon.value) return;
+    const pressed = event.key.toLowerCase();
+    if (isLetter(pressed) && !submittedLetters.includes(pressed)) {
+        submittedLetters.push(pressed)
+    }
 }
+onMounted(() => window.addEventListener("keypress", handleKeyPress))
+
 
 const reset = () => {
     currentLetter.value = ""
