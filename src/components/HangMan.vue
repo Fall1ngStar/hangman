c93681296f822940c1a0da04a4bf53bec710d86a
Fall1ngStar <12586447+Fall1ngStar@users.noreply.github.com>
2021-10-24 15:11:18 +0200

    add css

--- src/components/HangMan.vue
+++ src/components/HangMan.vue
@@ -1,11 +1,20 @@
 <template>
-    <h1>Hangman</h1>
+    <h1>Find the word</h1>
     <div>
-        <p>Find the word</p>
-        <p>{{ hiddenWord.join(" ") }}</p>
-        <p v-if="!hasWon">Failed letters: {{ failedLetter.join(" ") }}</p>
+        <p id="to-find">
+            <span :class="{ letter: letter !== '_' }" v-for="letter in hiddenWord">{{ letter }}</span>
+        </p>
+        <div v-if="!hasWon">
+            <p>Failed letters</p>
+            <p id="failed-letters">
+                <span class="failed-letter new-letter" v-for="letter in failedLetter">{{ letter }}</span>
+            </p>
+        </div>
         <p v-if="hasWon">You won !</p>
-        <p v-if="hasLost">You lost :(</p>
+        <div v-if="hasLost">
+        <p>You lost :(</p>
+        <p>The word was {{toFind}}</p>
+            </div>
         <button @click="reset">Reset game</button>
     </div>
 </template>
@@ -18,7 +27,7 @@ const currentLetter = ref("");
 const submittedLetters = reactive([]);
 
 const hiddenWord = computed(() => toFind.value.split("").map(letter => submittedLetters.includes(letter) ? letter : "_"))
-const failedLetter = computed(() => submittedLetters.filter(letter => !toFind.value.includes(letter)).sort())
+const failedLetter = computed(() => submittedLetters.filter(letter => !toFind.value.includes(letter)))
 const hasWon = computed(() => toFind.value.split("").filter(letter => !submittedLetters.includes(letter)).length === 0)
 const hasLost = computed(() => failedLetter.value.length >= 8)
 
@@ -40,3 +49,60 @@ const reset = () => {
 }
 
 </script>
+
+
+<style>
+body {
+    font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+    background-color: antiquewhite;
+    text-align: center;
+}
+#to-find {
+    font-size: 5em;
+    letter-spacing: 0.1em;
+}
+
+#failed-letters {
+    font-size: 3em;
+    letter-spacing: 0.2em;
+    color: red;
+}
+
+.letter {
+    animation-name: new-letter;
+    animation-duration: 0.25s;
+}
+
+.failed-letter {
+    animation-name: failed-letter;
+    animation-duration: 0.8s;
+    display: inline-block;
+}
+
+@keyframes new-letter {
+    from {
+        font-size: 1.5em;
+    }
+}
+
+@keyframes failed-letter {
+    0% {
+        transform: skewX(-15deg);
+    }
+    5% {
+        transform: skewX(15deg);
+    }
+    10% {
+        transform: skewX(-15deg);
+    }
+    15% {
+        transform: skewX(15deg);
+    }
+    20% {
+        transform: skewX(0deg);
+    }
+    100% {
+        transform: skewX(0deg);
+    }
+}
+</style>
